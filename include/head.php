cb8cdae2a28522ed08a4f38b032a8e2de5fde4b1
Marcelo Alves <marceloalvessoft@gmail.com>
2020-11-28 09:45:21 -0300

    Header Mozila

--- include/head.php
+++ include/head.php
@@ -4,27 +4,27 @@
      ?>               
     <meta charset="utf-8">
     <title>Portal Institucional 2021</title>
-    <link href="https://fonts.googleapis.com/csse2ff.css?family=Roboto+Condensed:400italic,700italic,400,700"  type='text/css'>
-    <link href="https://fonts.googleapis.com/css24e4.css?family=Roboto+Slab:400,300,100,700"  type='text/css'>
-    <link href="https://fontawesome.com/icons?d=gallery" type='text/css'>
-    <link type="text/css" href="css/jquery-ui.css" media="all" />
-    <link type="text/css" href="css/icomoon.css" media="all" />
-    <link type="text/css" href="css/common.css" media="all" />
-    <link type="text/css" href="css/superfish.css" media="screen" />
-    <link type="text/css" href="css/owl.carousel.css">
-    <link type="text/css" href="css/owl.theme.css">
-    <link type="text/css"  href="style.css" media="all">
-    <link  type="text/css" href="lib/lightbox/css/lightbox.css"></a>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/csse2ff.css?family=Roboto+Condensed:400italic,700italic,400,700"  type='text/css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css24e4.css?family=Roboto+Slab:400,300,100,700"  type='text/css'>
+    <link rel="stylesheet" href="https://fontawesome.com/icons?d=gallery" type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/icomoon.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/common.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/superfish.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+    <link rel="stylesheet" type="text/css"  href="style.css" media="all">
+    <link  rel="stylesheet" type="text/css" href="lib/lightbox/css/lightbox.css"></a>
     <!--link rel="stylesheet" href="lib/font-awesome/css/font-awesome.min.css?afver=400" rel="stylesheet" type="text/css" media="screen" /-->
     <!---coloquei em 17 set 2020 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     
     <!--[if lt IE 9]>
         <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-        <link href="css/ie.css" type="text/css" media="all" />
+        <link rel="stylesheet"  href="css/ie.css" type="text/css" media="all" />
                           <script src="js/PIE_IE678.js"></script>
     <![endif]-->
 
